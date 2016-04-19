5839235a5f95d1c6f248fa78221395cdc4aa795e
Sharoon Srivastava <srivas15@purdue.edu>
2016-04-19 02:36:35 -0400

    lol what

--- sharoon_file.py
+++ sharoon_file.py
@@ -2,7 +2,7 @@ import facebook
 
 
 
-graph = facebook.GraphAPI(access_token='CAACEdEose0cBAPM5CrLTXKHanGUMKxlYTKCBsyeY6l4W4So58ZBG0aOy82xnXAZAZAxvhKLKdLyhlHaQA5EDPCRojsb1fC7ZBobR4Lez2E4S0MGOUPj5SSXTxe20MpiCu2u2lVfqzhHANUTjdoU0H0S0o0WRcuV6t4ZBhdKZCL3T4kllIv7goabpTZBWjTpbIiT6NERg1JdntVLnug022r5', version='2.2')
+graph = facebook.GraphAPI(access_token='CAACEdEose0cBAO5eVN1qVLGtjl5qukQu8RkZAuHxw3Bcb6WQFZA5rl7rhLCCq2troNPPmiZCJoTqB5D5lRXLxVZAGpZCpJSC7gA0NJ7tDbpNhDNRFHZBfqew9NIGZBrBrYNzZCoHp9l0TGiVLMCZCuPjZBnDi6th9wOPnnLoO5v3u1PoBqTS5KFuFkgZBggAeSJ0LtHjfgVZA2HL0lnuRvQAE1d6', version='2.2')
 
 
 # f = open('test.txt', 'r')
@@ -13,7 +13,7 @@ graph = facebook.GraphAPI(access_token='CAACEdEose0cBAPM5CrLTXKHanGUMKxlYTKCBsye
 
 friend_list = []
 
-f = open('friends.txt', 'r');
+f = open('sharoon_friends.txt', 'r');
 str = f.read();
 #print str
 
@@ -26,7 +26,7 @@ while (x != -1):
 	x = x + 5;
 	y = str.find(",", x);
 	tokenid =  str[x:y]
-	print tokenid
+	#print tokenid
 	if (tokenid.startswith('"') and tokenid.endswith('"')):
 		tokenid = tokenid[1:-1]
 	friend_list.append(tokenid)
@@ -40,7 +40,13 @@ print(' ')
 print friend_list
 print len(friend_list)
 
+for i in friend_list:
+	person = graph.get_object(id=i)
+	print person['name']
+	print person['gender']
+	print ' '
 
+'''
 friends = graph.get_connections(id='me', connection_name='friends')
 
 print friends['name']
@@ -61,3 +67,4 @@ post = graph.get_object(id='post_id')
 print(post['message'])
 
 print "hey";
+'''
